d22cb41ed10fcdbd07c91bed3df77f6fe77a4ee3
Thang Phan <phanthq2k2@gmail.com>
2024-04-03 14:27:56 +0700

    init client-side using react

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container= styled.div`
+    height: 60px;
+`;
+
+const Wrapper= styled.div`
+    padding: 10px 20px;
+    display: flex;
+    justify-content: space-between;
+`;
+const Left= styled.div`
+    flex: 1;
+`;
+const Languaged= styled.div`
+    font-size: 14px;
+    cursor: pointer;
+`;
+const Center= styled.div`
+    flex: 1;
+`;
+
+const Right= styled.div`
+    flex: 1;
+`;
+
+const Navbar = () => {
+  return (
+    <Container>
+      <Wrapper>
+        <Left>
+            <Languaged>EN</Languaged>
+            <SearchContainer>
+                input
+                <Search>
+            </SearchContainer>
+        </Left>
+        <Center>Center</Center>
+        <Right>Right</Right>
+      </Wrapper>
+    </Container>
+  )
+}
+
+export default Navbar
