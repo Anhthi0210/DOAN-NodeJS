18b899bc2f91f2b6a1f70c66fdcc4f2de02b1fee
Thang Phan <phanthq2k2@gmail.com>
2024-04-08 10:15:22 +0700

    + login, register page + init Cart

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,12 @@
+import Cart from "./pages/Cart";
 import Home from "./pages/Home"
+import Login from "./pages/Login";
 import ProductDetails from "./pages/ProductDetails";
 import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
 
 const App = () => {
-  return <ProductDetails/>;
+  return <Cart/>;
 };
 
 export default App;
