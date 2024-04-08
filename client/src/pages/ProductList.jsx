a1329bf7f8b6cad7165ffb0c1b46721c2170e915
Thang Phan <phanthq2k2@gmail.com>
2024-04-08 08:07:53 +0700

    + init productPage

--- client/src/pages/ProductList.jsx
+++ client/src/pages/ProductList.jsx
@@ -1,21 +1,39 @@
 import styled from "styled-components"
 import Navbar from "../components/Navbar";
 import Announcement from "../components/Announcement";
-
+import Products from "../components/Products";
+import Newsletter from "../components/Newsletter";
+import Footer from "../components/Footer";
 
 const Container= styled.div`
 
 `;
 
 const Title= styled.h1`
-
+  margin: 20px;
 `;
 
 const FilterContainer= styled.div`
-
+  display: flex;
+  justify-content: space-between;
 `;
 
 const Filter= styled.div`
+  margin: 20px;
+`;
+
+const FilterText= styled.span`
+  font-size: 20px;
+  font-weight: 600;  
+  margin-right: 20px;
+`;
+
+const Select = styled.select`
+  padding: 10px;
+  margin-right: 20px;
+`;
+
+const Option = styled.option`
 
 `;
 
@@ -26,9 +44,42 @@ const ProductList = () => {
         <Navbar/>
         <Title>Sản phẩm</Title>
         <FilterContainer>
-            <Filter>filter1</Filter>
-            <Filter>filter2</Filter>
+        <Filter>
+          <FilterText>Lọc sản phẩm:</FilterText>
+          <Select>
+            <Option disabled selected>
+              Màu sắc
+            </Option>
+            <Option>Trắng</Option>
+            <Option>Đen</Option>
+            <Option>Đỏ</Option>
+            <Option>Xanh dương</Option>
+            <Option>Vàng</Option>
+            <Option>Xanh</Option>
+          </Select>
+          <Select>
+            <Option disabled selected>
+              Size
+            </Option>
+            <Option>XS</Option>
+            <Option>S</Option>
+            <Option>M</Option>
+            <Option>L</Option>
+            <Option>XL</Option>
+          </Select>
+        </Filter>
+        <Filter>
+          <FilterText>Sắp xếp sản phẩm:</FilterText>
+          <Select>
+            <Option selected>Mới nhất</Option>
+            <Option>Giá từ thấp đến cao</Option>
+            <Option>Giá từ cao xuống thấp</Option>
+          </Select>
+        </Filter>
         </FilterContainer>
+        <Products/>
+        <Newsletter/>
+        <Footer/>
     </Container>
   )
 }
